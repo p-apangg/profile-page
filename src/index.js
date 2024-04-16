84fcd64275d1a237a1c589ab9ff65442ec1681c0
Thanpimon Buamanee <30890150+p-apangg@users.noreply.github.com>
2024-04-17 02:11:53 +0900

    Add NavBar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import GreetingPage from './GreetingPage';
+import NavBar from './NavBar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavBar/>
     <GreetingPage/>
   </React.StrictMode>
 );
