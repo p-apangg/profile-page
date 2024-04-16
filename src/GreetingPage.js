0d1aae826583b2e6d92cd7f6cf9a0a87ae258117
Thanpimon Buamanee <30890150+p-apangg@users.noreply.github.com>
2024-04-16 18:44:56 +0900

    add GreetingPage

---
+++ src/GreetingPage.js
@@ -0,0 +1,25 @@
+import logo from './Images/GreetingPage.jpg';
+import './GreetingPage.css';
+
+function GreetingPage() {
+  return (
+    <div className="Greeting">
+      <header className="Greeting-header">
+        <img src={logo} className="circle-photo" alt="logo" />
+        <div className="Greeting-wording">
+        <p>
+          Hello, I am Papang!
+        </p>
+        <div>
+        I'm a first-year master's student pursuing Computer Science at Osaka University in Japan. 
+        Originally from Thailand, I'm passionate about technology and eager to share my journey with you.
+On this platform, I'll be showcasing my projects, sharing insights from my studies, and exploring the fascinating world of computer science.
+        </div>
+        <a href="mailto:thanpimon.bua@gmail.com" class="email-button">Contact Me!</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default GreetingPage;
